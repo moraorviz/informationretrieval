e500750c037bd40db2b3e89cc586727988d48c1d
mario <moraorviz@gmail.com>
2019-06-08 13:03:26 +0200

    refactor spearman module

--- app/common/spearman.py
+++ app/common/spearman.py
@@ -1,64 +1,44 @@
-# import our modules
-from .textrank_client import TextRankClient
-from .rootlog_client import RootLogClient
-from .logging_helper import logger
+'''A module for calculating the sperman coefficient of the used methods.'''
+
+
 import scipy as sp
+from scipy import stats
+
+import matplotlib.pyplot as plt
+
+from . import logging_helper as lh
+from . import persistence
+from . import utils
 
 
 class Spearman:
 
+    # Hard-coded data. Static class members/attributes.
+    TRWORDS_FILE = 'app/output/textrank.json'
+    RLWORDS_FILE = 'app/output/rootlog.json'
+
     def __init__(self):
-        logger.debug('Initializing %s.', self.__class__.__name__)
-        self.textrank_client = TextRankClient()
-        self.rootlog_client = RootLogClient()
-        self.textrank_output = None
-        self.rootlog_output = None
+        lh.logger.debug('Initializing %s.', self.__class__.__name__)
+        self.trwords = persistence.load_json(self.TRWORDS_FILE)
+        self.rlwords = persistence.load_json(self.RLWORDS_FILE) 
 
-    def set_outputs(self):
-        self.textrank_output = self.textrank_client.get_ordered_output()
-        self.rootlog_output = self.rootlog_client.get_ordered_output()
+    def get_common(self):
 
-    # Obtain two similar data structures from both methods in oder
-    # to compare them and find the words that are present of both.
-    def compare_outputs(self):
+        matchwords = set(self.trwords.keys()) & set(self.rlwords.keys())
+        textrank = []
+        rootlog = []
 
-        # Iterate through the lists. Check for words that appear
-        # in both lists.
-        words_t = []
-        words_r = []
-        # Gets only they keys (the words) from the output lists.
-        for i, v in self.textrank_output:
-            words_t.append(i)
-        for i, v in self.rootlog_output:
-            words_r.append(i)
+        for word, rank in self.trwords.items():
+            if word in matchwords:
+                tpl = (word, rank)
+                textrank.append(tpl)
 
-        matching_words = set(words_t) & set(words_r)
+        for word, rank in self.rlwords.items():
+            if word in matchwords:
+                tpl = (word, rank)
+                rootlog.append(tpl)
 
-        return matching_words
-
-    # Construct two separated lists of the matching words together
-    # with it's punctuation.
-    # TODO: refator repeated code.
-    def build_reduced_lists(self, words_set):
-
-        list_tr = []
-        list_rl = []
-
-        for word, rank in self.textrank_output:
-            if word in words_set:
-                w_tuple = (word, rank)
-                list_tr.append(w_tuple)
-
-        for word, rank in self.rootlog_output:
-            if word in words_set:
-                w_tuple = (word, rank)
-                list_rl.append(w_tuple)
-
-        return list_tr, list_rl
-
-    def spy(self, my_var):
-        print(type(my_var))
-        print(len(my_var))
+        return textrank, rootlog
 
     def calculate(self, a, b):
         rho, p_value = sp.stats.spearmanr(a, b)
@@ -66,7 +46,7 @@ class Spearman:
 
     # Builds a 2D vector with the scores from both methods.
     def build_input(self, list_tr, list_rl):
-        # Initialize variables.
+
         words = []
         tr_ranks = []
         lr_ranks = []
@@ -83,8 +63,32 @@ class Spearman:
         return tr_ranks, lr_ranks
 
     def get_values(self):
-        self.set_outputs()
-        matching_words_set = self.compare_outputs()
-        list_tr, list_rl = self.build_reduced_lists(matching_words_set)
-        x, y = self.build_input(list_tr, list_rl)
+        tr, rl = self.get_common()
+        x, y = self.build_input(tr, rl)
+
         return x, y
+
+class SpearmanClient:
+
+    SCATTER_PICTURE = 'app/images/spearman.png'
+
+    def __init__(self):
+        self.spearman = Spearman()
+
+    def get_plot(self, save=False):
+        x, y = self.spearman.get_values()
+        rho, p_value = self.spearman.calculate(x,y)
+        print(rho)
+        plt.scatter(x, y)
+        if save:
+            plt.savefig(self.SCATTER_PICTURE)
+        else:
+            plt.show()
+
+
+def main():
+    spcli = SpearmanClient()
+    spcli.get_plot(True)
+
+
+main()
